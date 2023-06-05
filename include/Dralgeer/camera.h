4dc1f1acc3857776b3d71865756c7ff456d9037b
Salamence064 <thomas.ducote64@gmail.com>
2023-06-05 09:26:02 -0500

    Camera is complete

--- include/Dralgeer/camera.h
+++ include/Dralgeer/camera.h
@@ -1,15 +1,33 @@
 #pragma once
 
 #include <GLM/glm/glm.hpp>
+#include <GLM/gtc/matrix_transform.hpp>
 
 namespace Dralgeer {
     struct Camera {
-        glm::mat4 projection, invProjection;
-        glm::mat4 view, invView;
         glm::vec2 pos;
-        glm::vec2 projectionSize = {1024, 576}; // 32 * 32, 32 * 18
+
+        glm::mat4 proj, invProj;
+        glm::mat4 view, invView;
+        
+        glm::vec2 projSize = {1024, 576}; // 32 * 32, 32 * 18
         float zoom = 1.0f;
 
-        // ! add adjustment functions
+        inline void adjustProjection() {
+            proj = glm::mat4(1.0f);
+            proj = glm::ortho(0.0f, projSize.x, 0.0f, projSize.y, 0.0f, 100.0f);
+            invProj = glm::inverse(proj);
+        };
+
+        inline void adjustView() {
+            view = glm::mat4(1.0f);
+            view = glm::lookAt(
+                glm::vec3(pos.x, pos.y, 20.0f),  // Set the camera a bit away from the screen's z so we can actually "see" the game
+                glm::vec3(pos.x, pos.y, -1.0f),  
+                glm::vec3(0.0f, 1.0f, 0.0f)      // Set the y-axis to be "up" because this is a 2D game engine
+            );
+
+            invView = glm::inverse(view);
+        };
     };
 }
