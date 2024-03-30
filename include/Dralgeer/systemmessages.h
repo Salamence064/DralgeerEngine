aa625092cfd9b2cddacdf4a0b7975602ce167277
Salamence064 <thomas.ducote64@gmail.com>
2024-03-30 11:34:05 -0500

    updated systemmessages to actually work

--- include/Dralgeer/systemmessages.h
+++ include/Dralgeer/systemmessages.h
@@ -2,37 +2,33 @@
 
 #pragma once
 
-// ? A header including macros for system messages
-// todo update to just be constexprs
+static constexpr void DEBUG_MESSAGE(std::string const &objectType, std::string const &name,
+        std::string const &description, int expected, int obtained)
+{
+    std::cout << "[DEBUG] " << objectType << " " << name << ":\n\t" << description << "\n\tExpected Value: " << expected << ". Obtained Value: " << obtained << "\n\t";
+    if(expected == obtained) {
+        std::cout << "[SUCCESS]\n";
+    
+    } else {
+        std::cout << "[FAILED]\n";
+    }
+};
 
+static constexpr int ERROR_MESSAGE(std::string const &objectType, std::string const &name,
+        std::string const &errorType, std::string const &description, int errorCode)
+{
+    std::cout << "[ERROR] " << objectType << " " << name << ":\n\t[" << errorType << "] " << description << "\n\tError Code " << errorCode << "\n";
+    return errorCode;
+};
 
-#define DEBUG_MESSAGE(objectType, name, description, expected, obtained) \
-({ \
-    std::cout << "[DEBUG] " << objectType << " " << name << ":\n\t" << description << "\n\tExpected Value: " << expected << ". Obtained Value: " << obtained << "\n\t"; \
-    if((expected) == (obtained)) { \
-        std::cout << "[SUCCESS]\n"; \
-    \
-    } else { \
-        std::cout << "[FAILED]\n"; \
-    } \
-    \
-    (expected) == (obtained) \
-})
+static constexpr void INFO_MESSAGE(std::string const &objectType, std::string const &name,
+        std::string const &description)
+{
+    std::cout << "[INFO] " << objectType << " " << name << ":\n\t" << description << "\n";
+};
 
-#define ERROR_MESSAGE(objectType, name, errorType, description, errorCode) \
-({ \
-    std::cout << "[ERROR] " << objectType << " " << name << ":\n\t[" << errorType << "] " << description << "\n\tError Code " << errorCode << "\n"; \
-    errorCode; \
-})
-
-
-#define INFO_MESSAGE(objectType, name, description) \
-({ \
-    std::cout << "[INFO] " << objectType << " " << name << ":\n\t" << description << "\n"; \
-    1; \
-})
-
-#define WARNING_MESSAGE(objectType, name, warningType, description, warningCode) \
-({ \
-    std::cout << "[WARNING] " << objectType << " " << name << ":\n\t[" << warningType << "] " << description << "\n\tWarning Code " << warningCode << "\n"; \
-})
+static constexpr void WARNING_MESSAGE(std::string const &objectType, std::string const &name,
+        std::string const &warningType, std::string const &description, int warningCode)
+{
+    std::cout << "[WARNING] " << objectType << " " << name << ":\n\t[" << warningType << "] " << description << "\n\tWarning Code " << warningCode << "\n";
+};
